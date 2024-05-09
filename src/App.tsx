5541d73cbe335389e8629311e69a618c7997c767
poli8512 <poli8512@gmail.com>
2024-05-09 07:19:04 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import styles from "./App.module.scss";
+console.log(styles);
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className={styles.test}>vkflvkflv;k</div>
+    </>
+  );
+}
+
+export default App;
